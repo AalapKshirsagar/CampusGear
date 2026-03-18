0ed417f75e592160a8060cfdfc1778403c877f52
AalapKshirsagar <r1115933@student.thomasmore.be>
2026-03-18 11:38:13 +0100

    Stap 11

--- src/main/java/be/thomasmore/campusgear/controllers/CampusController.java
+++ src/main/java/be/thomasmore/campusgear/controllers/CampusController.java
@@ -1,28 +1,45 @@
 package be.thomasmore.campusgear.controllers;
 
 import be.thomasmore.campusgear.model.Campus;
+import be.thomasmore.campusgear.model.Item;
 import be.thomasmore.campusgear.repositories.CampusRepository;
+import be.thomasmore.campusgear.repositories.ItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class CampusController {
 
     private final CampusRepository campusRepository;
+    private final ItemRepository itemRepository;
 
-    public CampusController(CampusRepository campusRepository) {
+    public CampusController(CampusRepository campusRepository,
+                            ItemRepository itemRepository) {
         this.campusRepository = campusRepository;
+        this.itemRepository = itemRepository;
     }
 
-    @GetMapping("/campusdetails")
-    public String campusDetails(Model model) {
-        Optional<Campus> campusFromDb = campusRepository.findById(1);
+    @GetMapping("/campuslist")
+    public String campusList(Model model) {
+        Iterable<Campus> campussen = campusRepository.findAll();
+        model.addAttribute("campussen", campussen);
+        return "campuslist";
+    }
+
+    @GetMapping("/campusitems/{campusId}")
+    public String campusItems(@PathVariable Integer campusId, Model model) {
+        Optional<Campus> campusFromDb = campusRepository.findById(campusId);
         if (campusFromDb.isPresent()) {
-            model.addAttribute("campus", campusFromDb.get());
+            Campus campus = campusFromDb.get();
+            List<Item> items = itemRepository.findByCampusId(campusId);
+            model.addAttribute("campus", campus);
+            model.addAttribute("items", items);
         }
-        return "campusdetails";
+        return "campusitems";
     }
 }
