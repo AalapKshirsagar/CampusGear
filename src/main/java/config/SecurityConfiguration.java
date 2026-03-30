c1adfdf32939f9b1c301b1a9661932aa22b76afd
AalapKshirsagar <r1115933@student.thomasmore.be>
2026-03-30 22:35:58 +0200

    Stap 21 - security aangepasrt

--- src/main/java/config/SecurityConfiguration.java
+++ src/main/java/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -12,7 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
@@ -48,11 +48,11 @@ public class SecurityConfiguration {
         http.logout(form -> form.logoutUrl("/logout"));
 
         if (h2ConsoleNeeded) {
-            http.csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console()));
+            http.csrf(csrf -> csrf
+                    .ignoringRequestMatchers("/h2-console/**"));
             http.headers(headers ->
                     headers.frameOptions(
-                            org.springframework.security.config.annotation.web.configurers
-                                    .HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+                            HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         }
 
         return http.build();
