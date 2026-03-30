1fb584df3ed311b717c9879f264be260e1a12d3c
AalapKshirsagar <r1115933@student.thomasmore.be>
2026-03-30 22:29:03 +0200

    Stap 20 - security

---
+++ src/main/java/be/thomasmore/campusgear/controllers/UserController.java
@@ -0,0 +1,32 @@
+package be.thomasmore.campusgear.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+        Authentication auth = SecurityContextHolder
+                .getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated()
+                && !auth.getName().equals("anonymousUser")) {
+            return "redirect:/itemlist";
+        }
+        return "user/login";
+    }
+
+    @GetMapping("/logout-page")
+    public String logoutPage() {
+        Authentication auth = SecurityContextHolder
+                .getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated()
+                || auth.getName().equals("anonymousUser")) {
+            return "redirect:/itemlist";
+        }
+        return "user/logout";
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS authorities;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+                       username varchar(50) not null primary key,
+                       password varchar(100) not null,
+                       enabled boolean not null
+);
+
+CREATE TABLE authorities (
+                             username VARCHAR(50) NOT NULL,
+                             authority VARCHAR(50) NOT NULL,
+                             FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
